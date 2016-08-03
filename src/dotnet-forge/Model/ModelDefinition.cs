a7a9420048f28fd753a8a2e51b04629ed071e1a4
Joshua Hampton <Joshua.Hampton@granicus.com>
2016-08-02 23:00:19 -0600

    separating definition from model creation part 1: definition

---
+++ src/dotnet-forge/Model/ModelDefinition.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Forge.Model
+{
+    public sealed class ModelDefinition
+    {
+        public string Namespace {get;set;}
+        public string ClassName {get;set;}
+        public IEnumerable<ModelProperty> Properties {get;set;}
+    }
+}
---
+++ src/dotnet-forge/Model/ModelProperty.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Forge.Model
+{
+    public sealed class ModelProperty
+    {
+        public string Name { get; set; }
+        public Type Type { get; set; }
+    }
+}
